55bb6dd7aa36476242b77ad4ba8117c4109b787a
utleroy <bryanhankins@me.com>
2016-03-02 21:06:38 -0600

    added mouseover event listener / made minor html changes

--- events.js
+++ events.js
@@ -1,6 +1,7 @@
 // write out variables on paper to start
 
 var output = document.getElementById("output-target");
+console.log(output);
 var keypress = document.getElementById("keypress-input");
 var colorBtn = document.getElementById("add-color");
 var hulkifyBtn = document.getElementById("make-large");
@@ -8,9 +9,23 @@ var captureItBtn = document.getElementById("add-border");
 var roundedBtn = document.getElementById("add-rounding");
 var header = document.getElementById("page-header");
 var title = document.getElementById("page-title");
+var guineaPig = document.getElementById("guinea-pig");
 
 // getElementsByClassName always returns array.
-var article = document.getElementByClassName("")
+var article = document.getElementsByClassName("article-section")
+
+// have to add event listener (click 5, mouseover 1, mouseout 2, )
+
+title.addEventListener("mouseover", function() {
+	output.innerHTML = "You moved your mouse over the header";
+  })
+
+
+// article.addEventListener("click", function() {
+// 	output.innerHTML = 
+// 	// going to return whatever is in the output-target <div>
+
+// })
 
 
 
