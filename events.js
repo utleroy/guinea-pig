ca71f5c219408f33cf0b270f08a9295bcaf72b98
utleroy <bryanhankins@me.com>
2016-03-02 19:51:59 -0600

    added beginning variables to js file

---
+++ events.js
@@ -0,0 +1,17 @@
+// write out variables on paper to start
+
+var output = document.getElementById("output-target");
+var keypress = document.getElementById("keypress-input");
+var colorBtn = document.getElementById("add-color");
+var hulkifyBtn = document.getElementById("make-large");
+var captureItBtn = document.getElementById("add-border");
+var roundedBtn = document.getElementById("add-rounding");
+var header = document.getElementById("page-header");
+var title = document.getElementById("page-title");
+
+// getElementsByClassName always returns array.
+var article = document.getElementByClassName("")
+
+
+
+
